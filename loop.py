29e7f35da2a6c9c7501b3ec3bf3936280315ed18
tushar nagar <115682174+Tusharnagar44@users.noreply.github.com>
2022-11-04 12:04:41 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,20 @@
+#loop through tuple
+#for loop
+tuplee=('apples','banana','oranges')
+#for x in tuplee:
+ #   print(x)
+
+#looping through index numbers
+#for i in range(len(tuplee)):
+ #              print(tuplee[i])
+
+#joining the tuple
+tuple1=(10,20,30,40)
+tuple2=(50,60,70,80)
+tuple3=tuple1+tuple2
+print(tuple3)
+
+#multiplication
+tuple4=["apple","banana","gauva"]
+mtuple=tuple4*2
+print(mtuple)
